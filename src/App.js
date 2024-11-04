8499da9d7f7534f0d153d0218f4480d3b9cac276
khasanboyurmonovdev <khasanboyurmonov1998@yahoo.com>
2024-11-04 21:55:51 +0900

    feat: Initial commit for Bookstore app project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// client/src/App.js
+import React from "react";
 
-function App() {
+import ProductList from "./components/ProductList";
+import Header from "./components/Header";
+import "./App.css"; // client/src/App.js
+
+import CustomItemContext from "./context/ItemContext";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CustomItemContext>
+      <Header />
+      <ProductList />
+    </CustomItemContext>
   );
-}
+};
 
 export default App;
